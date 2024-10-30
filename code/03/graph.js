cc3992c9c44e5ccb8d2155731cb737a93e0940fe
amunnelly <anthony.munnelly@gmail.com>
2024-10-30 18:59:22 +0000

    added directories 03 and 04, canvas and plot in 03, and scaling in 04

---
+++ code/03/graph.js
@@ -0,0 +1,33 @@
+function drawGraph(data) {
+  // DIMENSIONS
+  const width = 800;
+  const height = 600;
+
+  const margin = { top: 20, right: 20, bottom: 50, left: 50 };
+
+  const plot_width = width - margin.left - margin.right;
+  const plot_height = height - margin.left - margin.right;
+
+  var canvas = d3
+    .select("#canvas")
+    .append("svg")
+    .attr("height", height)
+    .attr("width", width)
+    .style("background", "aliceblue");
+
+  // PLOT
+  var plot = canvas
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.right})`);
+}
+
+function draw() {
+  d3.csv("data/iris.csv")
+    .then((data) => {
+      drawGraph(data);
+    })
+    .catch((err) => {
+      console.log("Something has gone wrong");
+      console.log(err);
+    });
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+Code samples from the d3-tutorial series on YouTube
